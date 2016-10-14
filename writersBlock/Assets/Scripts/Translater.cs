fbbb8f6836209eeee982a1b424498ee3d5034840
DADIU System PC <placeholder@dadiu.dk>
2016-10-14 16:05:26 +0200

    Updated Translater to prepare for spell implementation.

--- writersBlock/Assets/Scripts/Translater.cs
+++ writersBlock/Assets/Scripts/Translater.cs
@@ -4,26 +4,26 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 
-public class Translater : MonoBehaviour {
+public class Translater : MonoBehaviour
+{
 
-	public Message interpretMessage(string msg)
+    public Message interpretMessage(string msg)
     {
 
         msg = msg.ToLower();
 
-        string[] partMessages = msg.Split(' ');
+        string[] partMessages = msg.Split(' ').Where(s => !string.IsNullOrEmpty(s)).ToArray();
 
         string command = partMessages[0];
 
-        switch (command)
-        {
+        switch (command) {
             case "m":
             case "move":
                 return interpretMove(partMessages);
-            case "attack":            
+            case "attack":
                 return interpretAttack(partMessages);
             case "cast":
-                return interpretSpell(string.Join(" ", partMessages.Skip(1).ToArray()));
+                return interpretSpell(partMessages);
             default:
                 return new Message(false, "*WinkyFace*");
         }
@@ -36,45 +36,66 @@ public class Translater : MonoBehaviour {
 
     private Message interpretMove(string[] partMessages)
     {
-
         if (partMessages.Length <= 1)
             return new Message(false, "Specify a direction move you dumbnut");
 
         string direction = partMessages[1];
-        bool moveValidity;
-        switch (direction)
-        {
-            case "l":
-            case "left":
-                moveValidity = GameData.playerCharacter.move(new Vec2i(-1, 0));
-                break;
-            case "r":
-            case "right":
-                moveValidity = GameData.playerCharacter.move(new Vec2i(1, 0));
-                break;
-            case "u":
-            case "up":
-                moveValidity = GameData.playerCharacter.move(new Vec2i(0, 1));
-                break;
-            case "d":
-            case "down":
-                moveValidity = GameData.playerCharacter.move(new Vec2i(0, -1));
-                break;
-            default:
-                moveValidity = false;
-                break;
+        bool moveValidity = false;
+        try { 
+            moveValidity = GameData.playerCharacter.move(translateDirection(direction));
+        } catch (InvalidDirectionException ex) {
+            return new Message(false, ex.Message);
         }
 
         return new Message(moveValidity, "Slamming into a wall eh?");
     }
 
-    private Message interpretSpell(string rawIncantation)
+    private Message interpretSpell(string[] partMessages)
     {
+        if (partMessages.Length < 2)
+            return new Message(false, "Specify a direction!");
+        else if (partMessages.Length < 3)
+            return new Message(false, "Specify an incantation!");
+
+        string direction = partMessages[1]; // Translate this into a direction and spawn the spell
+        string incantation = string.Join(" ", partMessages.Skip(2).ToArray());
+
         IncantationBuilder ib = new IncantationBuilder();
-        ib.Expand(rawIncantation);
+        ib.Expand(incantation);
         var isValid = ib.HasValidElement && ib.HasValidType && ib.IsValidLanguage && !ib.IsRambling;
         return new Message(isValid, ib.ToString());
     }
+
+    private Vec2i translateDirection(string dir)
+    {
+        var dirVec = new Vec2i();
+        switch (dir) {
+            case "l":
+            case "left":
+                dirVec = new Vec2i(-1, 0); break;
+            case "r":
+            case "right":
+                dirVec = new Vec2i(1, 0); break;
+            case "u":
+            case "up":
+                dirVec = new Vec2i(0, 1); break;
+            case "d":
+            case "down":
+                dirVec = new Vec2i(0, -1); break;
+            default:
+                throw new InvalidDirectionException(string.Format("Cannot move in direction '{0}'; try directions 'up', 'down', 'left', 'right'", dir));
+        }
+
+        return dirVec;
+    }
+}
+
+public class InvalidDirectionException : Exception
+{
+    public InvalidDirectionException() : base() { }
+    public InvalidDirectionException(string message) : base(message) { }
+    public InvalidDirectionException(string message, Exception innerException) : base(message, innerException) { }
+    public InvalidDirectionException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
 
 public struct Message
